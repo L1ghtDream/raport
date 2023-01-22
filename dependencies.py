af633d3663e8dc708aad1f2720047fb36b6ea6c1
Radu Voinea <contact@lightdream.dev>
2023-01-22 15:12:51 +0200

    Updated dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -2,12 +2,10 @@ import os
 import json
 
 TRACKING_GROUPS=["dev.lightdream", "com.pokeninjas"]
-TMP_FOLDER = "tmp"
 DEPENDENCIES_OUTPUT = "dependencies.output"
 ARTIFACT_OUTPUT = "artifact.output"
 VERSION_OUTPUT = "version.output"
 GROUP = "group.output"
-WORKING_DIR = "run"
 PROCESS_DATA = "process.data"
 
 
@@ -20,16 +18,16 @@ def createFolder(folder):
 
 createFolder(f"{WORKING_DIR}/{TMP_FOLDER}")
 
-shell(f"./gradlew dependencies --configuration runtimeClasspath > {WORKING_DIR}/{TMP_FOLDER}/{DEPENDENCIES_OUTPUT}")
-shell(f"gradle properties -q | grep \"^name:\" | awk '{{print $2}}' > {WORKING_DIR}/{TMP_FOLDER}/{ARTIFACT_OUTPUT}")
-shell(f"gradle properties -q | grep \"^version:\" | awk '{{print $2}}' > {WORKING_DIR}/{TMP_FOLDER}/{VERSION_OUTPUT}")
-shell(f"gradle properties -q | grep \"^group:\" | awk '{{print $2}}' > {WORKING_DIR}/{TMP_FOLDER}/{VERSION_OUTPUT}")
+shell(f"./gradlew dependencies --configuration runtimeClasspath > {DEPENDENCIES_OUTPUT}")
+shell(f"gradle properties -q | grep \"^name:\" | awk '{{print $2}}' > {ARTIFACT_OUTPUT}")
+shell(f"gradle properties -q | grep \"^version:\" | awk '{{print $2}}' > {VERSION_OUTPUT}")
+shell(f"gradle properties -q | grep \"^group:\" | awk '{{print $2}}' > {VERSION_OUTPUT}")
 
-raw_dependencies = open(f"{WORKING_DIR}/{TMP_FOLDER}/{DEPENDENCIES_OUTPUT}", "r").read()
+raw_dependencies = open(f"{DEPENDENCIES_OUTPUT}", "r").read()
 
-project_artifact = open(f"{WORKING_DIR}/{TMP_FOLDER}/{ARTIFACT_OUTPUT}", "r").read().replace("\n", "")
-project_version = open(f"{WORKING_DIR}/{TMP_FOLDER}/{VERSION_OUTPUT}", "r").read().replace("\n", "")
-project_group = open(f"{WORKING_DIR}/{TMP_FOLDER}/{VERSION_OUTPUT}", "r").read().replace("\n", "")
+project_artifact = open(f"{ARTIFACT_OUTPUT}", "r").read().replace("\n", "")
+project_version = open(f"{VERSION_OUTPUT}", "r").read().replace("\n", "")
+project_group = open(f"{VERSION_OUTPUT}", "r").read().replace("\n", "")
 
 print("Artifact: " + project_artifact)
 print("Version: " + project_version)
@@ -79,4 +77,4 @@ payload = {
     }
 
 print(json.dumps(payload))
-open(f"{WORKING_DIR}/{PROCESS_DATA}", "w+").write(json.dumps(payload))
+open(f"{PROCESS_DATA}", "w+").write(json.dumps(payload))
