ae075eb2cd6d443571cc203ee4cb3226a5561865
Radu Voinea <contact@lightdream.dev>
2023-01-22 15:27:29 +0200

    Updated raport.sh

--- update_raport.sh
+++ update_raport.sh
@@ -1,12 +1,18 @@
+# usage [artifact] [version] [github_access_token]
+
+ARTIFACT=$1
+VERSION=$2
+TOKEN=$3
+
 wget https://raw.githubusercontent.com/L1ghtDream/raport/cdn/dependencies.py
 python3 dependencies.py
 git clone $RAPORT_URL raport
 cp process.data raport/process.data
 cd raport
 git add *
-git checkout -b "${{ env.ARTIFACT }}/${{ env.VERSION }}"
+git checkout -b "$ARTIFACT/$VERSION"
 git config --global user.email "bot@voinearadu.com"
 git config --global user.name "Radu Voinea [bot]"
-git commit -m "${{ env.ARTIFACT }}/${{ env.VERSION }}"
-git remote set-url origin https://L1ghtDream:${{ secrets.PERSONAL_TOKEN }}@github.com/L1ghtDream/raport
-git push --set-upstream origin ${{ env.ARTIFACT }}/${{ env.VERSION }}
+git commit -m "$ARTIFACT/$VERSION"
+git remote set-url origin https://L1ghtDream:$TOKEN@github.com/L1ghtDream/raport
+git push --set-upstream origin $ARTIFACT/$VERSION
