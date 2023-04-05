75a930e7b61cc912e1e0877eeaf9e9f2c52bddc8
Radu Voinea <contact@lightdream.dev>
2023-04-05 22:39:07 +0300

    Disabled report

--- update_report.sh
+++ update_report.sh
@@ -5,15 +5,18 @@ ARTIFACT=$2
 VERSION=$3
 TOKEN=$4
 
-wget https://raw.githubusercontent.com/L1ghtDream/raport/cdn/dependencies.py
-python3 dependencies.py
-git clone $RAPORT_URL raport
-cp process.data raport/process.data
-cd raport
-git add *
-git checkout -b "$ARTIFACT/$VERSION"
-git config --global user.email "bot@voinearadu.com"
-git config --global user.name "Radu Voinea [bot]"
-git commit -m "$ARTIFACT/$VERSION"
-git remote set-url origin https://L1ghtDream:$TOKEN@github.com/L1ghtDream/raport
-git push --set-upstream origin $ARTIFACT/$VERSION
+echo "Script disabled"
+
+
+# wget https://raw.githubusercontent.com/L1ghtDream/raport/cdn/dependencies.py
+# python3 dependencies.py
+# git clone $RAPORT_URL raport
+# cp process.data raport/process.data
+# cd raport
+# git add *
+# git checkout -b "$ARTIFACT/$VERSION"
+# git config --global user.email "bot@voinearadu.com"
+# git config --global user.name "Radu Voinea [bot]"
+# git commit -m "$ARTIFACT/$VERSION"
+# git remote set-url origin https://L1ghtDream:$TOKEN@github.com/L1ghtDream/raport
+# git push --set-upstream origin $ARTIFACT/$VERSION
