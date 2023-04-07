975c089739141c5f3ab0ed57011af812ac4ecf67
Radu Voinea <contact@lightdream.dev>
2023-04-07 17:26:28 +0300

    Updated crate_readme_template.sh

--- create_readme_template.sh
+++ create_readme_template.sh
@@ -2,7 +2,7 @@
 mkdir -p tmp
 cd tmp
 
-wget https://raw.githubusercontent.com/L1ghtDream/report/cdn/README-v2.md
+wget -O README-template-i1.md https://raw.githubusercontent.com/L1ghtDream/report/cdn/README-v2.md
 description=""
 
 cd ..
@@ -11,5 +11,5 @@ if [ -f "./readme/description.md" ]; then
     description=$(cat ./readme/description.md)
 fi
 
-cat ./tmp/README-template.md | sed "s/%description%/$description/g" > test.txt
+cat ./tmp/README-template-i1.md | sed "s/%description%/$description/g" > README-template.md
 
