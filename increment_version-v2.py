9d43e74387bebcf3e724828f38ca7f32954e1e47
Radu Voinea <contact@lightdream.dev>
2023-04-29 20:52:49 +0300

    New version increment script

---
+++ increment_version-v2.py
@@ -0,0 +1,22 @@
+# Requirements
+# - Have at least one of the files `gradle/libs.versions.toml` `build.gradle.kts` `build.gradle`
+# - Have at least one of them have either `project = "some_version"` or version = `"some_version"`
+
+import os
+
+os.system("./gradlew properties -q | grep \"^version:\" | awk \'{print $2}\' > tmp-current_version")
+
+version_file = open("tmp-current_version", "r")
+version_containers_files = ["gradle/libs.versions.toml", "build.gradle.kts", "build.gradle"]
+
+current_version = version_file.read().replace("\n", "").replace(" ", "")
+
+version_codes = current_version.split(".")
+
+version_codes[-1] = str(int(version_codes[-1]) + 1)
+
+new_version = ".".join(version_codes)
+
+for version_container_file in version_containers_files:
+    os.system(f"sed -i \"s/version = \\\"{current_version}\\\"/version = \\\"{new_version}\\\"/g\" {version_container_file}")
+    os.system(f"sed -i \"s/project = \\\"{current_version}\\\"/project = \\\"{new_version}\\\"/g\" {version_container_file}")
