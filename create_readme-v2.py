e1d70ca75ff857c26681693c311fbf591a98bf63
Radu Voinea <contact@lightdream.dev>
2023-04-29 21:18:20 +0300

    Creatge readme scripts

---
+++ create_readme-v2.py
@@ -0,0 +1,35 @@
+# Usage: python readme.py [repository] [artifact] [version]
+
+import os
+import sys
+
+DESCRIPTION_FILE = "readme/description.md"
+TEMPLATE_URL = "https://raw.githubusercontent.com/L1ghtDream/report/cdn/README-v3.md"
+README_TEMPLATE_FILE = "tmp-README-template.md"
+README_FILE = "README.md"
+
+os.system(f"wget -O {README_TEMPLATE_FILE} {TEMPLATE_URL}")
+
+description = ""
+
+if os.path.exists(DESCRIPTION_FILE):
+    with open(DESCRIPTION_FILE, "r") as file:
+        description = file.read()
+
+PLACEHOLDERS = {
+    "description": description,
+    "repository": sys.argv[1],
+    "artifact": sys.argv[2],
+    "version": sys.argv[3],
+}
+
+readme_template = ""
+
+with open(README_TEMPLATE_FILE, "r") as file:
+    readme_template = file.read()
+
+for key, value in PLACEHOLDERS.items():
+    readme_template = readme_template.replace(f"%{key}%", value)
+
+with open(README_FILE, "w+") as file:
+    file.write(readme_template)
